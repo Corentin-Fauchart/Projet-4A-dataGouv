927a994dd5b50930d6a239f86dcb7359b5c5b7ae
DroXalys <fauchart.corentin@gmail.com>
2021-12-18 23:15:25 +0100

    Ajout de notre tableau de data
    Nous avons un tableau qui affiche respectivement :
     - Réanimations
     - Hospitalisations
     - Retours à domicile
     - Décès à l'hopital
    
    Une liste permet de sélectionner le département pour lequel nous souhaitons afficher les données.
    
    + habillage de la page (css)
    + message d'introduction sur la page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
 
 
 var resDisplay = document.querySelector('#test');
+//C'est super bien ranger
+$.get("https://coronavirusapifr.herokuapp.com/data/live/departements", function(result){
+  //console.log(result);
+  console.log("ALLOOOOOOOO");
+  let jsonRequest = JSON.stringify(result);
+  let objRequest = JSON.parse(jsonRequest);
+  $("#test").text(jsonRequest);
+  
+  console.log(objRequest);
+  let selectedDep = $("#select_dep").val();
+    console.log(selectedDep);
+    $("#date_donnees").text(objRequest[selectedDep-1].date);
 
-$.get("https://coronavirusapifr.herokuapp.com/data/live/france", function(result){
-  console.log(result);
-  $("#test").text(JSON.stringify(result));
+    $("#region").text(objRequest[selectedDep-1].lib_reg);
+    $("#dep").text(objRequest[selectedDep-1].lib_dep);
+
+    $("#v1").text(objRequest[selectedDep-1].rea);
+    $("#v2").text(objRequest[selectedDep-1].hosp);
+    $("#v3").text(objRequest[selectedDep-1].rad);
+    $("#v4").text(objRequest[selectedDep-1].dchosp);
+
+  $("#select_dep").on('change', () =>{
+    let selectedDep = $("#select_dep").val();
+    console.log(selectedDep);
+    $("#date_donnees").text(objRequest[selectedDep-1].date);
+
+    $("#region").text(objRequest[selectedDep-1].lib_reg);
+    $("#dep").text(objRequest[selectedDep-1].lib_dep);
+
+    $("#v1").text(objRequest[selectedDep-1].rea);
+    $("#v2").text(objRequest[selectedDep-1].hosp);
+    $("#v3").text(objRequest[selectedDep-1].rad);
+    $("#v4").text(objRequest[selectedDep-1].dchosp);
+  })
+  
 })
-
-/*
-fetch("https://coronavirusapifr.herokuapp.com/data/live/france",{mode: "no-cors"}).then(function(response) {
-    response.json().then(function(text) {
-      resDisplay.html = text;
-    });
-  });
-
-  */
